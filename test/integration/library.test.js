aa6ae4c9f7801d56829db0ab903318f2f0d912e1
praveendvd <praveendvd@live.com>
2021-07-15 22:47:33 +0100

    Added ability to skip requests #308

--- test/integration/library.test.js
+++ test/integration/library.test.js
@@ -382,4 +382,17 @@ describe('Newman and htmlextra run from a script', function () {
             fs.stat(outFile, done);
         });
     });
+
+    it('Should correctly geenrate report for skip requests', function (done) {
+        newman.run({
+            collection: 'test/requests/simple-skipped-folder.json',
+            reporters: ['htmlextra'],
+            reporter: { htmlextra: { omitHeaders: true, export: outFile, skipRequests: 'request1,request4' } }
+        // eslint-disable-next-line consistent-return
+        }, function (err, summary) {
+            if (err) { return done(err); }
+            expect(summary.collection.name).to.equal('simple-skipped-folder');
+            fs.stat(outFile, done);
+        });
+    });
 });
